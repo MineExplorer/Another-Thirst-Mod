d99c2ae53b7cfb9a7f42e0f327598d5da6c6b2ec
MineExplorer <vladgrz40@gmail.com>
2019-11-24 14:56:49 +0300

    release

---
+++ ThirstMod/dev/HUD_bar.js
@@ -0,0 +1,18 @@
+IMPORT("ScalesRPG");
+
+let BitmapFactory = android.graphics.BitmapFactory;
+
+let thirstScale = new ScalesRPG.Scale({
+	right: true,
+    bitmaps:{
+        full: BitmapFactory.decodeFile(__dir__ + "gui/scale_water_0.png"), 
+        half: BitmapFactory.decodeFile(__dir__ + "gui/scale_water_1.png"),
+        empty: BitmapFactory.decodeFile(__dir__ + "gui/scale_water_2.png"),
+    }
+});
+
+Callback.addCallback("NativeGuiChanged", function(screenName) {
+    if(screenName == "hud_screen" || screenName == "in_game_play_screen"){
+        thirstScale.show();
+    }
+});
